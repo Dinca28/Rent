a13d6372a52df31c3b306ca00f090a81b73a7e24
Илко Илиев <iciservicebg@gmail.com>
2025-04-13 11:06:59 +0300

    Add create car

--- Controllers/CarsController.cs
+++ Controllers/CarsController.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Logging;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using RentACars.Models.Cars;
 
 namespace RentACars.Controllers
 {
@@ -32,70 +33,72 @@ namespace RentACars.Controllers
         [Authorize]
         public IActionResult PostCar()
         {
-            var carModel = new Car();
-            return View(carModel);
+            return View();
         }
 
         [HttpPost]
         [Authorize]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> PostCar(Car car, IFormFile image)
+        public async Task<IActionResult> PostCar(IFormCollection collection, IFormFile image)
         {
-            if (ModelState.IsValid)
+            var car = new Car();
+            car.Brand = collection["Brand"];
+            car.Model = collection["Model"];
+            car.Year = int.Parse(collection["Year"]);
+            car.PricePerDay = double.Parse(collection["PricePerDay"]);
+            car.Location = collection["Location"];
+
+
+            var userId = _userManager.GetUserId(User);
+            car.UserId = userId;
+
+            if (image != null && image.Length > 0)
             {
-                var userId = _userManager.GetUserId(User);
-                car.UserId = userId;
-
-                if (image != null && image.Length > 0)
-                {
-                    var uploadDir = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images");
-                    var fileName = Path.GetFileName(image.FileName);
-                    var filePath = Path.Combine(uploadDir, fileName);
-
-                    try
-                    {
-                        int i = 1;
-                        while (System.IO.File.Exists(filePath))
-                        {
-                            var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(image.FileName);
-                            var fileExtension = Path.GetExtension(image.FileName);
-                            fileName = $"{fileNameWithoutExtension}_{i}{fileExtension}";
-                            filePath = Path.Combine(uploadDir, fileName);
-                            i++;
-                        }
-
-                        using (var stream = new FileStream(filePath, FileMode.Create))
-                        {
-                            await image.CopyToAsync(stream);
-                        }
-
-                        car.ImageUrl = $"/images/{fileName}";
-                    }
-                    catch (Exception ex)
-                    {
-                        _logger.LogError("Грешка при качване на изображението: {Message}", ex.Message);
-                        ModelState.AddModelError("", "Грешка при качване на изображението.");
-                        return View(car);
-                    }
-                }
+                var uploadDir = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images");
+                var fileName = Path.GetFileName(image.FileName);
+                var filePath = Path.Combine(uploadDir, fileName);
 
                 try
                 {
-                    _context.Add(car);
-                    await _context.SaveChangesAsync();
-                    _logger.LogInformation("Кола успешно добавена: {Brand} {Model} ({Year}) от потребител с ID {UserId}", car.Brand, car.Model, car.Year, car.UserId);
+                    int i = 1;
+                    while (System.IO.File.Exists(filePath))
+                    {
+                        var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(image.FileName);
+                        var fileExtension = Path.GetExtension(image.FileName);
+                        fileName = $"{fileNameWithoutExtension}_{i}{fileExtension}";
+                        filePath = Path.Combine(uploadDir, fileName);
+                        i++;
+                    }
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await image.CopyToAsync(stream);
+                    }
+
+                    car.ImageUrl = $"/images/{fileName}";
                 }
                 catch (Exception ex)
                 {
-                    _logger.LogError("Грешка при записването на колата в базата: {Message}", ex.Message);
-                    ModelState.AddModelError("", "Грешка при добавяне на колата.");
+                    _logger.LogError("Грешка при качване на изображението: {Message}", ex.Message);
+                    ModelState.AddModelError("", "Грешка при качване на изображението.");
                     return View(car);
                 }
-
-                return RedirectToAction(nameof(Index));
             }
 
-            return View(car);
+
+            try
+            {
+                _context.Add(car);
+                await _context.SaveChangesAsync();
+                _logger.LogInformation("Кола успешно добавена: {Brand} {Model} ({Year}) от потребител с ID {UserId}", car.Brand, car.Model, car.Year, car.UserId);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Грешка при записването на колата в базата: {Message}", ex.Message);
+                ModelState.AddModelError("", "Грешка при добавяне на колата.");
+                return View(car);
+            }
         }
 
         public IActionResult Details(int id)
---
+++ Models/Cars/CreateCarViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace RentACars.Models.Cars
+{
+    public class CreateCarViewModel
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double PricePerDay { get; set; } // Ако използвате double за цена, няма нужда от конверсия
+        public string Location { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
